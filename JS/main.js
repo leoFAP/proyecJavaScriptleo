f4ce3b74c8fec58eaa6ea4511c786116574a0603
leoFAP <leonardoalegre2008@cesanrafael.org>
2024-11-19 12:25:57 -0400

    javasripkleo

---
+++ JS/main.js
@@ -0,0 +1,39 @@
+let mensaje= "hola mundo";
+
+function sumar(){
+let numero1=3;
+let numero2=4;
+let suma =numero1+numero2
+
+
+
+console.log("la suma es",suma);
+
+
+alert ("la suma es"+suma);
+}
+function restar(){
+    let numero1=3;
+    let numero2=4;
+    let restar =numero1-numero2
+    
+ alert ("la resta es"+restar)
+    }
+
+function multiplicar(){
+    let numero1=3;
+    let numero2=4;
+    let multiplicar =numero1*numero2
+    
+ alert ("la multiplicacion es"+multiplicar)
+ 
+    }
+    function division(){
+        let numero1=3;
+        let numero2=4;
+        let divicion =numero1/numero2
+        
+     alert ("la divicion es"+divicion)
+     
+        }
+    
